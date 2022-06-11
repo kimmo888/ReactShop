c3ea242adf77dbeae6c6067401685e3e4dd12185
Kastro888 <kasto777333@gmail.com>
2022-06-11 12:01:18 -0500

    Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import '../styles/Other.scss';
+import logo from '../../public/images/logos/logo_yard_sale.svg';
+import iconMenu from '../../public/images/icons/icon_menu.svg';
+import iconShoppingCar from '../../public/images/icons/icon_shopping_cart.svg';
+
+
+const Header = () => {
+    return (
+    <nav>
+        <div className="container-menu">
+            <img src={iconMenu} alt="menu" className="menu"/>
+        <div className="navbar-left">
+            <img src={logo} alt="logo" className="logo1"/>
+            <ul>
+                <li>
+                    <a href="/">all</a>
+                </li>
+                <li>
+                    <a href="/">Clothes</a>
+                </li>
+                <li>
+                    <a href="/">Electronics</a>
+                </li>
+                <li>
+                    <a href="/">Furnitures</a>
+                </li>
+                <li>
+                    <a href="/">Toys</a>
+                </li>
+                <li>
+                    <a href="/">Others</a>
+                </li>
+            </ul>
+        </div>
+        <div className="navbar-right">
+            <ul>
+                <li className="navbar-email">your@email.com</li>
+                <li className="navbar-shopping-car">
+                    <img src={iconShoppingCar} alt="shopping cart"/>
+                    <div>2</div>
+                </li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+    );
+}
+
+export default Header;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from '../containers/Layout';
 import Login from '../containers/Login';
 import RecoveryPassword from '../containers/RecoveryPassword';
-import Home from '../pages/Home.jsx';
-import NotFound from '../pages/NotFound.jsx';
+import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
 import '../styles/global.css';
 
 
