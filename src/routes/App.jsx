50124b8e48516fffdace2c0c6c1bd0c60c7667a7
Kastro888 <kasto777333@gmail.com>
2022-06-12 08:43:34 -0500

    App.jsx

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from '../containers/Layout';
-import Login from '../pages/NewPassword';
-import RecoveryPassword from '../pages/SendEmail';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
+import PasswordRecovery from '../pages/PasswordRecovery';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
 import NotFound from '../pages/NotFound';
 import '../styles/global.css';
 
 
 const App = () => {
-    return(
-        <BrowserRouter>
-            <Layout>
-                <Routes>
-                    <Route exact path='/' element={ <Home /> } />
-                    <Route exact path="/login" element={ <Login /> } />
-                    <Route exact path="/RecoveryPassword" element={ <RecoveryPassword  /> } />
-                    <Route path="*" element={ <NotFound /> } />
-                </Routes>
-            </Layout>
-        </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<Layout>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/password-recovery" component={PasswordRecovery} />
+					<Route exact path="/send-email" component={SendEmail} />
+					<Route exact path="/new-password" component={NewPassword} />
+					<Route exact path="/account" component={MyAccount} />
+					<Route exact path="/signup" component={CreateAccount} />
+					<Route exact path="/checkout" component={Checkout} />
+					<Route exact path="/orders" component={Orders} />
+					<Route path="*" component={NotFound} />
+				</Switch>
+			</Layout>
+		</BrowserRouter>
+	);
     {/* se puede abrir y cerrar en la misma etiqueta o podemos <Login></Login> o se puede ambas funcionan <Login/> , se deja un espacio para que con el llamado {children}, se añadan componentes.los nombres(<Layout>,<Login /> ) de etiquetas se crearon en la carpeta de containers y son llamadas de html, si no va a recibir ningún valor la estructura recomendada es <Login /> */}
 }
 export default App;
