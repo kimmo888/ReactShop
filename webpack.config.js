9a01e2edce413e1f435078155854325ad0ec34bf
Kastro888 <kasto777333@gmail.com>
2022-06-15 17:50:05 -0500

    fix webpack and pakage

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/',
+        filename: 'ReactShop/bundle.js',
+        publicPath: '/ReactShop/',
     },
     resolve: {
         extensions: ['.js', '.jsx'],
