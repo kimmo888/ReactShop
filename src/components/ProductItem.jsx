e17eca66f55571d7cf2fa30050a20547b5b2b911
Kastro888 <kasto777333@gmail.com>
2022-06-14 19:01:11 -0500

    useContext

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,12 +1,15 @@
-import React, {useState} from 'react';
+import React, {useContext} from 'react';
 import '../styles/Other.scss';
+import AppContext from '../context/AppContext';
 import ButtonAdd from '@icon/bt_add_to_cart.svg';
 
 const ProductItem = ( {product} ) => { // poner el parámetro a iterar en este caso product que es el resultado de el hooks useGetProducts.js
-    const [cart, setCart] = useState([]); // se declara el valor inicial del elemento setCart, los valores por defecto son vacíos, por que si no se muestra en la web con el valor que se ponga.
+    //?const [cart, setCart] = useState([]); //este es de useState, pero se cambio por useContext
+    // se declara el valor inicial del elemento setCart, los valores por defecto son vacíos, por que si no se muestra en la web con el valor que se ponga.
+    const { addToCart } = useContext(AppContext);
 
-    const handleClick = () => { // por, sobre el evento  onClick
-        setCart([]);
+    const handleClick = item => { // por, sobre el evento  onClick
+        addToCart(item);
     }
     const { id, title, price, images } = product; //forma para desestructurar los props de react en este caso product.
 
@@ -18,7 +21,7 @@ const ProductItem = ( {product} ) => { // poner el parámetro a iterar en este c
                         <p>$ {price} </p>
                         <p> {title} </p>
                     </div>
-                    <figure onClick={handleClick}>
+                    <figure onClick={()=> handleClick(product)}>
                         <img src={ButtonAdd} alt="button Add" />
                     </figure>
                 </div>
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -11,27 +11,32 @@ import CreateAccount from '../pages/CreateAccount';
 import Checkout from '../pages/Checkout';
 import Orders from '../pages/Orders';
 import NotFound from '../pages/NotFound';
+import AppContext from '../context/AppContext';
+import useInitialState from '../hooks/useInitialState';
 import '../styles/global.css';
 
 
 const App = () => {
+    const initialState = useInitialState();
     return(
-        <BrowserRouter>
-            <Layout>
-                <Routes>
-                    <Route exact path='/' element={ <Home /> } />
-                    <Route exact path='/Login' element={ <Login /> } />
-                    <Route exact path='/PasswordRecovery' element={ <PasswordRecovery /> } />
-                    <Route exact path="/SendEmail" element={ <SendEmail  /> } />
-                    <Route exact path="/NewPassword" element={ <NewPassword /> } />
-                    <Route exact path='/Account' element={ <MyAccount /> } />
-                    <Route exact path='/Signup' element={ <CreateAccount /> } />
-                    <Route exact path='/Checkout' element={ <Checkout /> } />
-                    <Route exact path='/Orders' element={ <Orders /> } />
-                    <Route path="*" element={ <NotFound /> } />
-                </Routes>
-            </Layout>
-        </BrowserRouter>
+        <AppContext.Provider value={initialState}>
+            <BrowserRouter>
+                <Layout>
+                    <Routes>
+                        <Route exact path='/' element={ <Home /> } />
+                        <Route exact path='/Login' element={ <Login /> } />
+                        <Route exact path='/PasswordRecovery' element={ <PasswordRecovery /> } />
+                        <Route exact path="/SendEmail" element={ <SendEmail  /> } />
+                        <Route exact path="/NewPassword" element={ <NewPassword /> } />
+                        <Route exact path='/Account' element={ <MyAccount /> } />
+                        <Route exact path='/Signup' element={ <CreateAccount /> } />
+                        <Route exact path='/Checkout' element={ <Checkout /> } />
+                        <Route exact path='/Orders' element={ <Orders /> } />
+                        <Route path="*" element={ <NotFound /> } />
+                    </Routes>
+                </Layout>
+            </BrowserRouter>
+        </AppContext.Provider>
     );
     {/* se puede abrir y cerrar en la misma etiqueta o podemos <Login></Login> o se puede ambas funcionan <Login/> , se deja un espacio para que con el llamado {children}, se añadan componentes.los nombres(<Layout>,<Login /> ) de etiquetas se crearon en la carpeta de containers y son llamadas de html, si no va a recibir ningún valor la estructura recomendada es <Login /> */}
 }
