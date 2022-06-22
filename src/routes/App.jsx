8df6a46458aecec38f50e005dca1a837746b4ec9
Kastro888 <kasto777333@gmail.com>
2022-06-22 18:02:32 -0500

    other Deploy

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
     const initialState = useInitialState(); //agregamos esta linea
     return(
         <AppContext.Provider value={initialState}> {/* damos el valor initialState */}
-            <BrowserRouter > {/* basename='/ReactShop/'  */}
+            <BrowserRouter basename='/ReactShop/' > {/* basename='/ReactShop/'  */}
                 <Layout>
                     <Routes>
                         <Route exact path='/' element={ <Home /> } />
